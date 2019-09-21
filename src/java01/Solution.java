fa2dabe30cc8124c855246040d253364d6a38bcb
shubham dixit <shubham.dixit_iot18@gla.ac.in>
2019-09-21 21:30:59 +0530

    create get and set method.

--- src/java01/Solution.java
+++ src/java01/Solution.java
@@ -5,6 +5,30 @@ public class Solution {
     private String authorname;
     private long ISBN;
 
+    public String getBookname() {
+        return bookname;
+    }
+
+    public void setBookname(String bookname) {
+        this.bookname = bookname;
+    }
+
+    public String getAuthorname() {
+        return authorname;
+    }
+
+    public void setAuthorname(String authorname) {
+        this.authorname = authorname;
+    }
+
+    public long getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(long ISBN) {
+        this.ISBN = ISBN;
+    }
+
 }
 
 
